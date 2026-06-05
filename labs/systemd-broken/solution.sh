01c2f2645d223bb70a2f9094c793c33ebde35a5c
HimanM <67066047+HimanM@users.noreply.github.com>
2026-06-06 02:43:02 +0530

    fix: resolve carriage return failure in systemd ExecStart

--- labs/systemd-broken/solution.sh
+++ labs/systemd-broken/solution.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Remove any carriage returns from the service file first
+sed -i 's/\r//g' /etc/systemd/system/demo.service
+
 # Edit the ExecStart line in demo.service to point to /usr/local/bin/demo.sh
 sed -i 's|ExecStart=/usr/local/bin/demo-service.sh|ExecStart=/usr/local/bin/demo.sh|' /etc/systemd/system/demo.service
 
