be0fbd97abb96cb31bbfea3d0a01fe6900b5217e
HimanM <67066047+HimanM@users.noreply.github.com>
2026-06-08 12:00:16 +0530

    fix: ensure rate limiting phase is not bypassed

--- labs/nginx-rate-limiting/solution.sh
+++ labs/nginx-rate-limiting/solution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Increase the rate and add a burst parameter
-sed -i 's/rate=1r\/s/rate=10r\/s/' /etc/nginx/conf.d/rate_limit.conf
+sed -i 's/rate=1r\/m/rate=10r\/s/' /etc/nginx/conf.d/rate_limit.conf
 sed -i 's/limit_req zone=mylimit;/limit_req zone=mylimit burst=10 nodelay;/' /etc/nginx/sites-available/default
 
 # Restart nginx
